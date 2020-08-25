09adbd7d4ded580d30fa3bf3f3bf20c4fe5e0d07
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-08-25 14:43:15 +0200

    vanskelig med syntax

--- manifests/baseconfig/packages.pp
+++ manifests/baseconfig/packages.pp
@@ -5,9 +5,9 @@ class profile::baseconfig::packages {
   $pkgs = lookup('profile::baseconfig::packages', {
     'value_type' => Array[String],
     'merge'      => 'unique',
-  }
+  })
 
-  ensure_packages($pkgs, {
+  ensure_packages( $pkgs, {
     'ensure' => 'present',
-  )}
+  })
 }
