a79768998803296102acabf7588055712b3a192a
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-08-26 12:36:39 +0200

    Ensure tftp directories and files is created

--- manifests/services/tftp.pp
+++ manifests/services/tftp.pp
@@ -2,6 +2,10 @@
 class profile::services::tftp {
   $rootdir = '/var/lib/tftpboot/'
 
+  file { $rootdir:
+    ensure => 'directory',
+  }
+
   class { '::tftp':
     directory => $rootdir,
     options   => '--secure',
@@ -11,13 +15,22 @@ class profile::services::tftp {
     ensure => 'present',
   }
 
-  file { "${rootdir}pxelinux.0":
+  tftp::file { 'pxelinux.cfg':
+    ensure => 'directory',
+  }
+
+  tftp::file { 'pxelinux.cfg/default':
+    ensure => 'file',
+    source => 'puppet:///modules/profile/pxelinux.cfg/default',
+  }
+
+  tftp::file { 'pxelinux.0':
     ensure  => 'file',
     source  => '/usr/lib/PXELINUX/pxelinux.0',
     require => Package['pxelinux'],
   }
 
-  file { "${rootdir}ldlinux.c32":
+  tftp::file { 'ldlinux.c32':
     ensure  => 'file',
     source  => '/usr/lib/syslinux/modules/bios/ldlinux.c32',
     require => Package['syslinux'],
