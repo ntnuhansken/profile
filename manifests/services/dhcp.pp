c8c3a6e7477da7c2536ca55953724cf38b5543b7
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-08-26 11:11:55 +0200

    Add dhcp profile

---
+++ manifests/services/dhcp.pp
@@ -0,0 +1,44 @@
+# Installs and configures a DHCP server
+class profile::services::dhcp {
+  $searchdomain = lookup('profile::dhcp::searchdomain', Stdlib::Fqdn)
+  $ntp_servers = lookup('profile::ntp::servers', {
+    'value_type' => Array[Variant[Stdlib::Fqdn, Stdlib::IP::Address]],
+    'merge'      => 'unique',
+  })
+  $interfaces = keys(lookup('profile::baseconfig::network::interfaces', Hash))
+  $dhcp_interfaces = lookup('profile::dhcp::server::interfaces', {
+    'value_type'    => Array[String],
+    'default_value' => $interfaces,
+  })
+  $networks = lookup('profile::networks', {
+    'value_type' => Array[String],
+    'merge'      => 'unique',
+  })
+
+  $man_if = $facts['netowrking']['primary']
+  $mip = $facts['networking']['interfaces'][$man_if]['ip']
+  $pxe_server = lookup('profile::dhcp::pxe::server', {
+    'value_type'    => Stdlib::IP::Address::V4,
+    'default_value' => $mip,
+  })
+  $pxe_file = lookup('profile::dhcp::pxe::file', {
+    'value_type'    => String,
+    'default_value' => 'pxelinux.0',
+  })
+
+  $nameservers = lookup('profile::dns::resolvers', {
+    'value_type' => Array[Stdlib::IP::Address::V4],
+    'merge'      => 'unique',
+  })
+
+  class { '::dhcp':
+    dnssearchdomains => [$searchdomain],
+    interfaces       => $dhcp_interfaces,
+    nameservers      => $nameservers,
+    ntpservers       => $ntp_servers,
+    pxeserver        => $pxe_server,
+    pxefilename      => $pxe_file,
+  }
+
+  profile::services::dhcp::pool { $networks: }
+}
---
+++ manifests/services/dhcp/pool.pp
@@ -0,0 +1,22 @@
+# Creates DHCP pool based on data from hiera
+define profile::services::dhcp::pool {
+  $id = lookup("profile::networks::${name}::ipv4::id")
+  $domain = lookup("profile::networks::${name}::domain")
+  $mask = lookup("profile::networks::${name}::ipv4::mask")
+  $gateway = lookup("profile::networks::${name}::ipv4::gateway", {
+    'default_value' => '',
+    'value_type'    => Variant[Stdlib::IP::Address::V4, String],
+  })
+  $range = lookup("profile::networks::${name}::ipv4::dynamicrange", {
+    'default_value' => '',
+    'value_type'    => Variant[Array[String], String],
+  })
+
+  ::dhcp::pool { $name:
+    network     => $id,
+    mask        => $mask,
+    range       => $range,
+    gateway     => $gateway,
+    domain_name => $domain,
+  }
+}
