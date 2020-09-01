2ec7a7e8746f1aad0a728db45d9698acbd6516cc
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-09-01 10:16:00 +0200

    Added libvirt service

---
+++ manifests/services/libvirt.pp
@@ -0,0 +1,36 @@
+# Installs KVM/libvirt
+class profile::services::libvirt {
+
+  $nets = lookup('profile::kvm::networks', {
+    'default_value' => false,
+    'value_type'    => Variant[Hash, Boolean],
+  })
+
+  if($nets) {
+    $networks = $nets.reduce({}) | $memo, $n | {
+      $netname = $n[0]
+      $dev = $nets[$netname]['bridge']
+      $memo + {
+        'bridge'      => $netname,
+        'forward_dev' => $dev,
+      }
+    }
+  } else {
+    $networks = {}
+  }
+
+  class { '::libvirt':
+    drop_default_net => true,
+    create_networks  => $networks,
+  }
+
+  libvirt_pool { 'default':
+    ensure => absent,
+  }
+  libvirt_pool { 'vmvg':
+    autostart => true,
+    type      => 'logical',
+    sourcedev => ['/dev/sdb'],
+    target    => '/dev/vmvg',
+  }
+}
