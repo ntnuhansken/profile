74e2bf29e1096ac4243e4f09831f84a0a1169268
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-09-14 12:49:10 +0200

    Add /32 in fw rule for munin servers

--- manifests/services/munin/node/firewall.pp
+++ manifests/services/munin/node/firewall.pp
@@ -2,8 +2,12 @@
 class profile::services::munin::node::firewall {
   $muninservers = lookup('profile::munin::servers')
 
+  $muninserverscidr = $muninservers.map | $server | {
+    $server + '/32'
+  }
+
   ::profile::baseconfig::firewall::custom { 'munin':
-    source   => $muninservers,
+    source   => $muninserverscidr,
     port     => 4949,
     protocol => 'tcp'
   }
