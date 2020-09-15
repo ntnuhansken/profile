4369ae7df0765a95b71ab26490fb3de16c0c7dcf
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-09-15 14:00:01 +0200

    Change default listen address for mysql

--- manifests/services/mysql.pp
+++ manifests/services/mysql.pp
@@ -4,10 +4,17 @@ class profile::services::mysql {
   include ::profile::services::mysql::firewall
 
   $root_pw = lookup('profile::mysql::root_password', String)
+  $nic = $facts['networking']['primary']
+  $ip  = $facts['networking']['interfaces'][$nic]['ip']
 
   class { '::mysql::server':
     root_password           => $root_pw,
     restart                 => true,
     remove_default_accounts => true,
+    override_options        => {
+      mysqld => {
+        'bind-address' => $ip,
+      }
+    }
   }
 }
