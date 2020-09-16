c6fa07e4d9d2db81bd6a4cb3211fff8b9ee45069
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-09-16 13:15:40 +0200

    legg til db conf kanskje

--- manifests/services/zabbix/server.pp
+++ manifests/services/zabbix/server.pp
@@ -4,14 +4,17 @@ class profile::services::zabbix::server {
   include ::profile::services::zabbix::firewall
 
   $zabbix_url = lookup('profile::zabbix::url')
+  $db_pass = lookup('profile::zabbix:database::password')
   $version = lookup('profile::zabbix::version', {
     'default_value' => '5.1',
     'value_type'    => String,
   })
 
   class { '::zabbix':
-    database_type  => 'mysql',
-    zabbix_url     => $zabbix_url,
-    zabbix_version => $version,
+    database_type     => 'mysql',
+    zabbix_url        => $zabbix_url,
+    zabbix_version    => $version,
+    database_user     => 'zabbix',
+    database_password => $db_pass,
   }
 }
