b8e5ebfe3e92845422743c01f954a959d2e50319
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-09-16 12:57:03 +0200

    Set default zabbix version

--- manifests/services/zabbix/agent.pp
+++ manifests/services/zabbix/agent.pp
@@ -2,9 +2,14 @@
 class profile::services::zabbix::agent {
 
   $zabbix_ip = lookup('profile::zabbix::server::ip')
+  $version = lookup('profile::zabbix::version', {
+    'default_value' => '5.1',
+    'value_type'    => String,
+  })
 
   class { '::zabbix::agent':
     server          => $zabbix_ip,
+    zabbix_version  => $version,
     agent_use_ip    => true,
     manage_firewall => true,
   }
--- manifests/services/zabbix/server.pp
+++ manifests/services/zabbix/server.pp
@@ -4,9 +4,14 @@ class profile::services::zabbix::server {
   include ::profile::services::zabbix::firewall
 
   $zabbix_url = lookup('profile::zabbix::url')
+  $version = lookup('profile::zabbix::version', {
+    'default_value' => '5.1',
+    'value_type'    => String,
+  })
 
   class { '::zabbix':
-    database_type => 'mysql',
-    zabbix_url    => $zabbix_url,
+    database_type  => 'mysql',
+    zabbix_url     => $zabbix_url,
+    zabbix_version => $version,
   }
 }
