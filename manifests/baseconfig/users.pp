7b8ca2659b8ccecbd828db79f4a0d74c22f205bd
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2020-10-23 15:12:03 +0200

    More modern user management

--- manifests/baseconfig/users.pp
+++ manifests/baseconfig/users.pp
@@ -1,17 +1,53 @@
-# Add users
+# Create users
 class profile::baseconfig::users {
-
   $users = lookup('profile::users', {
-    'value_type'    => Variant[Array[String], Boolean],
-    'default_value' => false,
+    'value_type'    => Variant[Hash, Boolean],
+    'default_value' => false
   })
-
-  if($users) {
-    profile::baseconfig::createuser { $users: }
-  }
 
   group { 'users':
     ensure => present,
     gid    => 700,
+  }
+
+  if($users) {
+    $users.each | $user, $data | {
+      if($data['ensure']) { $ensure = $data['ensure'] }
+      else                { $ensure = 'present' }
+
+      user { $user:
+        ensure         => $ensure,
+        gid            => 'users',
+        groups         => $data['groups'],
+        home           => "/home/${user}",
+        managehome     => true,
+        password       => $data['hash'],
+        purge_ssh_keys => true,
+        require        => Group['users'],
+        shell          => '/bin/bash',
+        uid            => $data['uid'],
+      }
+
+      if($ensure == 'present') {
+        file { "/home/${user}/.ssh":
+          ensure  => 'directory',
+          owner   => $user,
+          group   => 'users',
+          mode    => '0700',
+          require => User[$user],
+        }
+      }
+
+      if($data['ssh-keys']) {
+        $data['ssh-keys'].each | $keyname, $key_data | {
+          ssh_authorized_key { $keyname:
+            user    => $user,
+            type    => $key_data['type'],
+            key     => $key_data['key'],
+            require => File["/home/${user}/.ssh"],
+          }
+        }
+      }
+    }
   }
 }
