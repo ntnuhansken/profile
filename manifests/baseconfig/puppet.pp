5f3036954688da3b98bece83feee155ad9c8a91d
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2021-12-02 09:06:56 +0100

    Prepare puppet-agent conf for puppet7

--- manifests/baseconfig/puppet.pp
+++ manifests/baseconfig/puppet.pp
@@ -2,21 +2,25 @@
 class profile::baseconfig::puppet {
   $environment = lookup('profile::puppet::environment', String)
   $puppetserver = lookup('profile::puppet::hostname', Stdlib::Fqdn)
+  $caserver = lookup('profile::puppet::caserver', Stdlib::Fqdn)
   $agentconfigfile = '/etc/puppetlabs/puppet/puppet.conf'
-  $agentpackage = 'puppet-agent'
+  $runinterval = lookup('profile::puppet::runinterval', {
+    'default_value' => '30m',
+    'value_type'    => String,
+  })
 
-  package { $agentpackage:
-    ensure => 'present',
-  }
+  $collection = lookup('profile::puppet::collection', {
+    'value_type' => String,
+  })
 
-  ini_setting { 'Puppet environment':
-    ensure  => present,
-    path    => $agentconfigfile,
-    section => 'agent',
-    setting => 'environment',
-    value   => $environment,
-    notify  => Service['puppet'],
-    require => Package[$agentpackage],
+  $config = [
+    {'section' => 'agent', 'setting' => 'environment', 'value' => $environment},
+    {'section' => 'agent', 'setting' => 'runinterval', 'value' => $runinterval},
+  ]
+
+  class { 'puppet_agent':
+    config     => $config,
+    collection => $collection,
   }
 
   ini_setting { 'Puppet server':
@@ -25,13 +29,12 @@ class profile::baseconfig::puppet {
     section => 'agent',
     setting => 'server',
     value   => $puppetserver,
-    notify  => Service['puppet'],
-    require => Package[$agentpackage],
   }
-
-  service { 'puppet':
-    ensure  => 'running',
-    enable  => true,
-    require => Package[$agentpackage],
+  ini_setting { 'Puppet caserver':
+    ensure  => present,
+    path    => $agentconfigfile,
+    section => 'agent',
+    setting => 'ca_server',
+    value   => $caserver,
   }
 }
